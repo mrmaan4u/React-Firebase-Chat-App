6b3ad53265ae81ba700b4a87254f93ec911d06f6
Gurujot Singh <mrmaan4u@gmail.com>
2024-07-20 17:29:34 +1000

    Auto Scroll using useRef Hook

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./chat.scss";
 import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
 const [open, setOpen] = useState(false);
 const [text, setText] = useState("");
+
+const endRef = useRef(null)
+
+useEffect(()=>{
+    endRef.current?.scrollIntoView({ behaviour: "smooth"});
+}, []);
 
 const handleEmoji = e => {
  setText((prev) => prev + e.emoji);
@@ -55,6 +61,7 @@ const handleEmoji = e => {
                 <span>1 min ago</span>
                 </div>
             </div>
+            <div ref={endRef}></div>
         </div>
         <div className="bottom">
             <div className="icons">
